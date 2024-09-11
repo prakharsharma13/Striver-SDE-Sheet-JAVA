6060805fec0da6b1440d682f70f36b1f3ef60284
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-09-11 10:59:56 +0530

    14 longest common prefix

---
+++ Strings/LongestCommonPrefix.java
@@ -0,0 +1,22 @@
+import java.util.Arrays; // Importing the Arrays package
+
+public class LongestCommonPrefix {
+    public static String longestCommonPrefix(String[] v) {
+        StringBuilder ans = new StringBuilder();
+        Arrays.sort(v);
+        String first = v[0];
+        String last = v[v.length - 1];
+        for (int i = 0; i < Math.min(first.length(), last.length()); i++) {
+            if (first.charAt(i) != last.charAt(i)) {
+                return ans.toString();
+            }
+            ans.append(first.charAt(i));
+        }
+        return ans.toString();
+    }
+
+    public static void main(String[] args) {
+        String[] v = {"flower", "flow", "flight"}; // Corrected array initialization
+        System.out.println(longestCommonPrefix(v));
+    }
+}
